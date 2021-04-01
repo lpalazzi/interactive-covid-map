c5bf07b57d6488cc6e8993467972d3e38effc0e4
lpalazzi <lpalazzi@outlook.com>
2021-04-01 16:57:25 -0400

    drop unused columns from covid data and load in HR mappings

--- src/main.py
+++ src/main.py
@@ -1,11 +1,16 @@
 import pandas as pd
 import geopandas as gpd
 
-# load health unit boundary data
-health_units = gpd.read_file('./data/health-units/lhrp000b06a_e_Oct2011.shp')
+# load health region boundary data
+health_regions = gpd.read_file('./data/health-units/lhrp000b06a_e_Oct2011.shp')
+
+# load health region mappings
+health_region_mappings = pd.read_csv('./data/covid-data/other/hr_map.csv')
 
 # load covid data
 covid_data_2020 = pd.read_csv('./data/covid-data/individual_level/cases_2020.csv')
 covid_data_2021 = pd.read_csv('./data/covid-data/individual_level/cases_2021.csv')
 covid_data = pd.concat([covid_data_2020, covid_data_2021])
 
+# clean covid data
+covid_data = covid_data.drop(['country', 'travel_yn', 'travel_history_country', 'locally_acquired', 'case_source', 'additional_info', 'additional_source', 'method_note'], axis=1)
