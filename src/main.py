c3c557e811b7d87bf2a3779634e7a8e101ee2b57
lpalazzi <lpalazzi@outlook.com>
2021-04-05 10:42:33 -0400

    merge covid_data with health_region_mappings to get HR_UID

--- src/main.py
+++ src/main.py
@@ -14,3 +14,6 @@ covid_data = pd.concat([covid_data_2020, covid_data_2021])
 
 # clean covid data
 covid_data = covid_data.drop(['country', 'travel_yn', 'travel_history_country', 'locally_acquired', 'case_source', 'additional_info', 'additional_source', 'method_note'], axis=1)
+
+# map covid_data.health_region name to health_region_mappings.HR_UID
+covid_data = pd.merge(covid_data, health_region_mappings, how='left', on=['health_region', 'province'])
